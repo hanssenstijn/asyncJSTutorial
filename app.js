bbdf1145da6f3458f941220103a7ed43454d162b
hanssenstijn <hanssenstijn@gmail.com>
2020-07-15 16:58:01 +0200

    callback

--- app.js
+++ app.js
@@ -2,14 +2,15 @@
 console.log('Start');
 
 
-function loginUser(email, password) {
+function loginUser(email, password, callback) {
   setTimeout(() => {
     console.log('Now we have the data');
-    return { userEmail: email };
+    callback({ userEmail: email });
   }, 1500);
 }
 
-const user = loginUser('hanssenstijn@gmail.com', 123456);
-console.log(user)
+const user = loginUser('hanssenstijn@gmail.com', 123456, (user) => {
+  console.log(user);
+});
 
 console.log('End');
