77bef66164f006878e3e8ca49553c1583f8e8cca
hanssenstijn <hanssenstijn@gmail.com>
2020-07-16 15:02:27 +0200

    reject

--- promise.js
+++ promise.js
@@ -1,10 +1,11 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
     console.log('got the user');
-    resolve({ user: 'stijn' });
+    reject(new Error('User not logged in'));
   }, 2000);
 });
 
 promise.then(user => {
   console.log(user);
-});
+}).catch(err => console.log(err.message));
+
