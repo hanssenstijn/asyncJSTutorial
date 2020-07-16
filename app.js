8acf836ec581283e3ca166dc102f161eabf75595
hanssenstijn <hanssenstijn@gmail.com>
2020-07-16 09:51:33 +0200

    callbacks

--- app.js
+++ app.js
@@ -9,8 +9,26 @@ function loginUser(email, password, callback) {
   }, 1500);
 }
 
+function getUserVideos(email, callback) {
+  setTimeout(() => {
+    callback(['video1', 'video2', 'video3']);
+  }, 1000);
+}
+
+function videoDetails(video, callback) {
+  setTimeout(() => {
+    callback('title of the video');
+  }, 1000);
+}
+
 const user = loginUser('hanssenstijn@gmail.com', 123456, (user) => {
   console.log(user);
+  getUserVideos(user.userEmail, videos => {
+    console.log(videos);
+    videoDetails(video[0], (title) => {
+      console.log(title);
+    })
+  });
 });
 
 console.log('End');
