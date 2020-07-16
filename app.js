7cb2ba815e8e25cfeda61b0d2127c26ec713f4a0
hanssenstijn <hanssenstijn@gmail.com>
2020-07-16 15:40:25 +0200

    promise.all

--- app.js
+++ app.js
@@ -37,3 +37,20 @@ loginUser('S', 'H')
     console.log(detail));
 
 console.log('End');
+
+const yt = new Promise(resolve => {
+  setTimeout(() => {
+    console.log('getting stuff from youtube');
+    resolve({ videos: [1, 2, 3, 4, 5] });
+  }, 2000)
+});
+
+const fb = new Promise(resolve => {
+  setTimeout(() => {
+    console.log('getting stuff from facebook');
+    resolve({ user: 'Name' });
+  }, 2000)
+});
+
+Promise.all([yt, fb])
+  .then(result => console.log(result));
